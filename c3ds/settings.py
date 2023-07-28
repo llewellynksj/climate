7a9b41bd05bcee72d61388ea09d7042aadc87e92
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-07-28 11:33:22 +0000

    Deployment push

--- c3ds/settings.py
+++ c3ds/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '8000-llewellynksj-climate-4h1zh7jhcon.ws-eu102.gitpod.io']
+ALLOWED_HOSTS = ['localhost', '8000-llewellynksj-climate-4h1zh7jhcon.ws-eu102.gitpod.io', 'c3ds.herokuapp.com']
 
 
 # Application definition
@@ -80,13 +81,16 @@ WSGI_APPLICATION = 'c3ds.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
